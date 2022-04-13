f792e86cd205618db0d2b438ab1a529810e78f6a
Kolja Bruns <Kolja-bruns@gmx.de>
2022-04-13 22:21:20 +0200

    clean

--- apps/qcar_customer/test/env_test.dart
+++ apps/qcar_customer/test/env_test.dart
@@ -3,7 +3,7 @@ import 'package:qcar_customer/core/environment_config.dart';
 
 void main() {
   test('check default env config', () async {
-    expect(EnvironmentConfig.APP_NAME, "Car manual");
+    expect(EnvironmentConfig.APP_NAME, "QCar");
     expect(EnvironmentConfig.ENV, Env.DEV.name);
     expect(EnvironmentConfig.FLAVOR, "");
   });
