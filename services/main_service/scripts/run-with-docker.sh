080e242214151a9ee9ed414923c191e321c1f22e
Kolja Bruns <Kolja-bruns@gmx.de>
2022-06-06 17:26:50 +0200

    update docker file

--- services/main_service/scripts/run-with-docker.sh
+++ services/main_service/scripts/run-with-docker.sh
@@ -1,14 +1,31 @@
 #run in service folder - sh scripts/run-with-docker.sh
 
-SERVICE_VERSION="${1:-0.0.1}"
+SERVICE_VERSION="${1:-0.0.5}"
 SERVICE_NAME="$(basename $(pwd))"
 PORT=8088
 JAR_PATH="build/libs/${SERVICE_NAME}-${SERVICE_VERSION}.jar"
+CREDENTIALS_JSON="qcar-firebase-adminsdk.json"
+CREDENTIALS_PATH="src/main/resources/"
+CREDENTIALS=$CREDENTIALS_PATH$CREDENTIALS_JSON
 
 #SERVICE_VERSION must be the same as in build.gradle
 ./gradlew build &&
-docker build --build-arg JAR_FILE="$JAR_PATH" -t "$SERVICE_NAME" . &&
-IMAGE_ID=$(docker images --filter="reference=$SERVICE_NAME" --quiet) &&
-docker run -p "$PORT":"$PORT" "IMAGE_ID" &&
+docker build \
+--build-arg JAR_FILE="$JAR_PATH" \
+--build-arg CREDENTIALS_FILE="$CREDENTIALS" \
+-t "$SERVICE_NAME" .
+
+IMAGE_ID=$(docker images --filter="reference=$SERVICE_NAME" --quiet)
+#docker run -p "$PORT":"$PORT" "$IMAGE_ID" &&
 #docker run -d -p ${PORT}:${PORT} "IMAGE_ID" --host 0.0.0.0 &&
-docker ps
+#docker ps
+
+docker run \
+-p 9090:${PORT} \
+-e PORT=${PORT} \
+-e K_SERVICE=dev \
+-e K_CONFIGURATION=dev \
+-e K_REVISION=dev-00001 \
+-e GOOGLE_APPLICATION_CREDENTIALS="credentials.json" \
+-v $GOOGLE_APPLICATION_CREDENTIALS:"credentials.json":ro \
+"$IMAGE_ID"
