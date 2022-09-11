14cf6f7fe1a10878da7b1ad33ac8c4fc95770c89
Kolja Bruns <Kolja-bruns@gmx.de>
2022-09-11 19:54:26 +0200

    feedback model in backend fixed

--- services/main_service/src/main/kotlin/de/bitb/main_service/models/Feedback.kt
+++ services/main_service/src/main/kotlin/de/bitb/main_service/models/Feedback.kt
@@ -3,6 +3,6 @@ package de.bitb.main_service.models
 import de.bitb.main_service.parseDateString
 import java.time.LocalDate
 
-data class Feedback(val date: String = "", val text: String = "") {
+data class Feedback(val date: String = "", val text: String = "", val rating: Int = 0) {
     fun dateAsDateTime(): LocalDate = parseDateString(date)
 }
--- services/main_service/src/main/kotlin/de/bitb/main_service/models/Tracking.kt
+++ services/main_service/src/main/kotlin/de/bitb/main_service/models/Tracking.kt
@@ -3,6 +3,6 @@ package de.bitb.main_service.models
 import de.bitb.main_service.parseDateString
 import java.time.LocalDate
 
-data class Tracking(val date: String = "", val text: String = "") {
+data class Tracking(val date: String = "", val type: String = "", val text: String = "") {
     fun dateAsDateTime(): LocalDate = parseDateString(date)
 }
