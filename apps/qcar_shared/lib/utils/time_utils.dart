d55482fa00a4de3a6068daa3b60c885b883de3a0
Kolja Bruns <Kolja-bruns@gmx.de>
2022-11-03 22:23:13 +0100

    init sharing code

---
+++ apps/qcar_shared/lib/utils/time_utils.dart
@@ -0,0 +1,25 @@
+const MILLI_PER_SEC = 1000;
+const SEC_PER_MIN = MILLI_PER_SEC * 60;
+const MIN_PER_HOUR = SEC_PER_MIN * 60;
+const HOUR_PER_DAY = MIN_PER_HOUR * 24;
+const DAY_PER_WEEK = HOUR_PER_DAY * 7;
+const DAY_PER_MONTH_30 = HOUR_PER_DAY * 30;
+const DAY_PER_MONTH_31 = HOUR_PER_DAY * 31;
+const DAY_PER_YEAR = HOUR_PER_DAY * 365;
+
+// Helper functions that not only awaits a future but also ensures
+// that we wait at least for specific time
+Future<T> atLeast<T>(
+  Future<T> computation, {
+  Duration duration = const Duration(seconds: 3),
+}) async {
+  final minTime = Future.delayed(duration);
+  try {
+    final T result = await computation;
+    await minTime;
+    return result;
+  } catch (e) {
+    await minTime;
+    rethrow;
+  }
+}
