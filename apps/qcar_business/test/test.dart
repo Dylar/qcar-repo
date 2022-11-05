dd6c5d9a23497df09cd4d5d423487d2fd4165ced
Kolja Bruns <Kolja-bruns@gmx.de>
2022-11-05 14:31:29 +0100

    Init business app

---
+++ apps/qcar_business/test/test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  test('Just for testing', () async {
+    expect(true, isTrue);
+  });
+}
